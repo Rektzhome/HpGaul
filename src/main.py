1bd8c10cc476a3f2b814e6250fb60b364aa46267
Rektzhome <hitungyuk.myid@gmail.com>
2025-05-03 08:44:09 -0400

    Configure port for Render deployment

--- src/main.py
+++ src/main.py
@@ -46,4 +46,5 @@ def serve(path):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5002, debug=False)
+    port = int(os.environ.get("PORT", 5002))
+    app.run(host=\'0.0.0.0\', port=port, debug=False)
